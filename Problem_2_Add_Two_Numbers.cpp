eb91ccd0fa1288e6e35ca1e8b798d73f26962c99
fyog <eric.alex.ross@gmail.com>
2025-06-12 08:23:05 -0600

    first commit

---
+++ Problem_2_Add_Two_Numbers.cpp
@@ -0,0 +1,67 @@
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode* answer = new ListNode();
+        ListNode* list = answer;
+        int carry = 0;
+        int sum = 0;
+
+        // while neither list is empty
+        while (l1 != nullptr || l2 != nullptr) {
+
+            // if both lists are not empty
+            if (l1 != nullptr && l2 != nullptr) {
+                
+                // if there was a carry value left over from a previous iteration
+                if (carry > 0) {
+                    sum = l1 -> val + l2 -> val + 1; // sum the two list node values together, plus 1 for the carry value
+                    carry = 0; // reset the carry value
+                } else { 
+                    sum = l1 -> val + l2 -> val; // sum the list node values together
+                }
+                if (sum > 9) carry++; // if the sum is greater than 9, increment the carry value
+            
+            // if list 2 is empty
+            } else if (l1 != nullptr && l2 == nullptr) {
+                
+                // if there was a carry value left over from a previous iteration
+                if (carry > 0) {
+                    sum = l1 -> val + 1; // the sum is the value of the list 1 node, plus 1 for the carry value
+                    carry = 0; 
+                } else {
+                    sum = l1 -> val; // the sum is the value of the list 1 node
+                }
+                if (sum > 9) carry++; // if the sum is greater than 9, increment the carry value
+            } else {
+                if (carry > 0) {
+                    sum = l2 -> val + 1; // the sum is the value of the list 1 node, plus 1 for the carry value
+                    carry = 0;
+                } else {
+                sum = l2 -> val; // the sum is the value of the list 1 node
+                }
+                if (sum > 9) carry++; // if the sum is greater than 9, increment the carry value         
+            }
+
+            // move to the next node in the list
+            list -> next = new ListNode(sum % 10); // mod 10 the sum when creating the next node (digits can only range from 0 to 9)
+            list = list -> next; 
+            if (l1 != nullptr) l1 = l1 -> next;
+            if (l2 != nullptr) l2 = l2 -> next;
+        }
+        if (carry > 0) {
+            list -> next = new ListNode(1);
+        }
+    
+        return answer -> next;
+    }
+};
